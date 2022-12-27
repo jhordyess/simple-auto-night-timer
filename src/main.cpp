3c5ba9f185c9992a5b4150a689a097d94f18c926
Jhordy Gavinchu <dev@jhordyess.com>
2022-12-27 03:37:42 +0000

    Resolve bugs

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,26 @@
+
 #include <Arduino.h>
 #include <RTClib.h>
+#include <SPI.h>
 
 RTC_DS3231 rtc;
-const relayPin = 6;
+int relayPin = 6;
 
-void setup(){
+void setup() {
   Serial.begin(9600);
-  if(!rtc.begin()) {
+  if (!rtc.begin()) {
     Serial.println("Couldn't find RTC!");
     Serial.flush();
-    while (1) delay(10);
+    while (1)
+      delay(10);
   }
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, LOW);
 }
 
-void loop(){
+void loop() {
   DateTime now = rtc.now();
-  
+
   Serial.print(now.year(), DEC);
   Serial.print('/');
   Serial.print(now.month(), DEC);
@@ -31,12 +34,12 @@ void loop(){
   Serial.print(':');
   Serial.print(now.second(), DEC);
   Serial.println();
-  
-  if(now.hour() == 18 && now.minute() == 30){
+
+  if (now.hour() == 18 && now.minute() == 30) {
     digitalWrite(relayPin, HIGH);
   } else {
     digitalWrite(relayPin, LOW);
   }
-  
+
   delay(1000);
 }
