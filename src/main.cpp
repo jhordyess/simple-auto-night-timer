8f74e63efa59e808a7c336573de634232e2d93c4
jhordyess <dev@jhordyess.com>
2023-12-12 12:40:56 -0400

    Updated with LCD, improved readability

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,101 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 #include <RTClib.h>
 #include <SPI.h>
 
-RTC_DS3231 rtc;
-int relayPin = 8;
+const int LCD_ADDRESS = 0x3F;
+const int LCD_COLUMNS = 16;
+const int LCD_ROWS = 2;
+const int RELAY_PIN = 8;
+const int MINIMUM_HOUR = 19;
+const int MAXIMUM_HOUR = 22;
 
-boolean compare(DateTime now) {
-  return now.hour() >= 19 || now.hour() <= 23;
+RTC_DS3231 realTimeClock;
+LiquidCrystal_I2C lcdDisplay(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
+
+boolean isHourInRange(DateTime currentTime) {
+  int currentHour = currentTime.hour();
+  return (currentHour >= MINIMUM_HOUR && currentHour < MAXIMUM_HOUR);
+}
+
+void initializeLCD() {
+  lcdDisplay.init();
+  lcdDisplay.backlight();
+  lcdDisplay.setCursor(0, 0);
+  lcdDisplay.print("Initializing...");
+  delay(500);
+  lcdDisplay.clear();
+}
+
+void initializeRTC() {
+  lcdDisplay.setCursor(0, 0);
+  if (!realTimeClock.begin()) {
+    lcdDisplay.print("Time unknown!");
+    Serial.println("Time unknown!");
+    Serial.flush();
+    while (1)
+      delay(10);
+  } else {
+    lcdDisplay.print("Time ok");
+    Serial.println("Time ok");
+    delay(500);
+  }
+
+  lcdDisplay.clear();
+  lcdDisplay.setCursor(0, 0);
+  if (realTimeClock.lostPower()) {
+    lcdDisplay.print("RTC lost power");
+    Serial.println("RTC lost power");
+    realTimeClock.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+  realTimeClock.disable32K();
 }
 
 void setup() {
   Serial.begin(9600);
-  if (!rtc.begin()) {
-    Serial.println("Couldn't find RTC!");
-    Serial.flush();
-    while (1)
-      delay(10);
-  }
-  // rtc.adjust(DateTime(__DATE__, __TIME__));
-  pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, HIGH);
+  initializeLCD();
+  initializeRTC();
+
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);
+
+  lcdDisplay.setCursor(0, 0);
+  lcdDisplay.print("Ready to go!");
+  lcdDisplay.setCursor(0, 1);
+  lcdDisplay.print("Turn at " + String(MINIMUM_HOUR) + " - " + String(MAXIMUM_HOUR));
+  Serial.println("Ready to go!");
+  Serial.println("Turn at " + String(MINIMUM_HOUR) + " - " + String(MAXIMUM_HOUR));
+  delay(1000);
 }
 
 void loop() {
-  DateTime now = rtc.now();
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(' ');
+  lcdDisplay.clear();
+  lcdDisplay.setCursor(0, 0);
 
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
+  char date[16] = "DDD,DD-MMM-YYYY";
+  char time[9] = "hh:mm:ss";
+  DateTime currentTime = realTimeClock.now();
+  currentTime.toString(date);
+  currentTime.toString(time);
 
-  if (compare(now)) {
-    digitalWrite(relayPin, LOW);
+  lcdDisplay.setCursor(0, 0);
+  lcdDisplay.print(date);
+  lcdDisplay.setCursor(0, 1);
+  lcdDisplay.print(time);
+  Serial.println(date);
+  Serial.println(time);
+
+  if (isHourInRange(currentTime)) {
+    digitalWrite(RELAY_PIN, LOW);
+    lcdDisplay.setCursor(11, 1);
+    lcdDisplay.print("ON");
+    Serial.println("ON");
   } else {
-    digitalWrite(relayPin, HIGH);
+    digitalWrite(RELAY_PIN, HIGH);
+    lcdDisplay.setCursor(11, 1);
+    lcdDisplay.print("OFF");
+    Serial.println("OFF");
   }
-  delay(1000);
+  delay(500);
 }
