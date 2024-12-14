7f39dadfb8249d4181b82837b6ae17c625421d45
jhordyess <dev@jhordyess.com>
2024-12-13 20:45:10 -0400

    Refactor RTCManager date and time methods to remove seconds parameter for simplicity

--- lib/RTCManager/RTCManager.cpp
+++ lib/RTCManager/RTCManager.cpp
@@ -32,18 +32,17 @@ bool RTCManager::isHourInRange(int minimumHour, int maximumHour) {
   return (currentHour >= minimumHour && currentHour < maximumHour);
 }
 
-void RTCManager::getDateTime(int *year, int *month, int *day, int *hour, int *minute, int *second) {
+void RTCManager::getDateTime(int *year, int *month, int *day, int *hour, int *minute) {
   DateTime currentTime = rtc.now();
   *year = currentTime.year();
   *month = currentTime.month();
   *day = currentTime.day();
   *hour = currentTime.hour();
   *minute = currentTime.minute();
-  *second = currentTime.second();
 }
 
-void RTCManager::setDateTime(int year, int month, int day, int hour, int minute, int second) {
-  rtc.adjust(DateTime(year, month, day, hour, minute, second));
+void RTCManager::setDateTime(int year, int month, int day, int hour, int minute) {
+  rtc.adjust(DateTime(year, month, day, hour, minute, 0));
 }
 
 void RTCManager::startStopWatch() {
--- lib/RTCManager/RTCManager.h
+++ lib/RTCManager/RTCManager.h
@@ -13,8 +13,8 @@ public:
   void getCurrentDateTime(char *date, char *time);
   bool isHourInRange(int minimumHour, int maximumHour);
 
-  void getDateTime(int *year, int *month, int *day, int *hour, int *minute, int *second);
-  void setDateTime(int year, int month, int day, int hour, int minute, int second);
+  void getDateTime(int *year, int *month, int *day, int *hour, int *minute);
+  void setDateTime(int year, int month, int day, int hour, int minute);
 
   void startStopWatch();
   void resetStopWatch();
