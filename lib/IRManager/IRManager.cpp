4c39c717ee8e4ac0b8bf76a6dc7100069035a687
jhordyess <dev@jhordyess.com>
2024-12-18 16:39:59 -0400

    Refactor IRManager to simplify button command handling and improve initialization

--- lib/IRManager/IRManager.cpp
+++ lib/IRManager/IRManager.cpp
@@ -1,11 +1,9 @@
 #include "IRManager.h"
-
-// #define USE_IRREMOTE_HPP_AS_PLAIN_INCLUDE
 #include <IRremote.hpp>
 
-IRManager::IRManager(int recvPin) : recvPin(recvPin) {}
+IRManager::IRManager() {}
 
-void IRManager::initialize() {
+void IRManager::initialize(int recvPin) {
   IrReceiver.begin(recvPin, true);
 }
 
@@ -13,78 +11,10 @@ bool IRManager::decode() {
   return IrReceiver.decode();
 }
 
-bool IRManager::isCommand(uint8_t command) {
+bool IRManager::isCommand(Command command) {
   return IrReceiver.decodedIRData.command == command;
 }
 
 void IRManager::resume() {
   IrReceiver.resume();
-}
-
-bool IRManager::isBtn1() {
-  return isCommand(BTN_1);
-}
-
-bool IRManager::isBtn2() {
-  return isCommand(BTN_2);
-}
-
-bool IRManager::isBtn3() {
-  return isCommand(BTN_3);
-}
-
-bool IRManager::isBtn4() {
-  return isCommand(BTN_4);
-}
-
-bool IRManager::isBtn5() {
-  return isCommand(BTN_5);
-}
-
-bool IRManager::isBtn6() {
-  return isCommand(BTN_6);
-}
-
-bool IRManager::isBtn7() {
-  return isCommand(BTN_7);
-}
-
-bool IRManager::isBtn8() {
-  return isCommand(BTN_8);
-}
-
-bool IRManager::isBtn9() {
-  return isCommand(BTN_9);
-}
-
-bool IRManager::isBtnAsterisk() {
-  return isCommand(BTN_ASTHERISK);
-}
-
-bool IRManager::isBtn0() {
-  return isCommand(BTN_0);
-}
-
-bool IRManager::isBtnHash() {
-  return isCommand(BTN_HASH);
-}
-
-bool IRManager::isBtnUp() {
-  return isCommand(BTN_UP);
-}
-
-bool IRManager::isBtnLeft() {
-  return isCommand(BTN_LEFT);
-}
-
-bool IRManager::isBtnOk() {
-  return isCommand(BTN_OK);
-}
-
-bool IRManager::isBtnRight() {
-  return isCommand(BTN_RIGHT);
-}
-
-bool IRManager::isBtnDown() {
-  return isCommand(BTN_DOWN);
 }
--- lib/IRManager/IRManager.h
+++ lib/IRManager/IRManager.h
@@ -1,54 +1,33 @@
 #ifndef IRMANAGER_H
 #define IRMANAGER_H
 
+enum Command {
+  BTN_1 = 0x45,
+  BTN_2 = 0x46,
+  BTN_3 = 0x47,
+  // BTN_4 = 0x44,
+  // BTN_5 = 0x40,
+  // BTN_6 = 0x43,
+  // BTN_7 = 0x07,
+  // BTN_8 = 0x15,
+  // BTN_9 = 0x09,
+  BTN_ASTHERISK = 0x16,
+  // BTN_0 = 0x19,
+  // BTN_HASH = 0x0D,
+  BTN_UP = 0x18,
+  BTN_LEFT = 0x08,
+  BTN_OK = 0x1C,
+  BTN_RIGHT = 0x5A,
+  BTN_DOWN = 0x52
+};
+
 class IRManager {
 public:
-  // Due the library is imported in the .cpp file, we need to declare this typedef here
-  typedef unsigned int uint8_t __attribute__((__mode__(__QI__)));
-  IRManager(int recvPin);
-  void initialize();
-  void resume();
+  IRManager();
+  void initialize(int recvPin);
   bool decode();
-  bool isBtn1();
-  bool isBtn2();
-  bool isBtn3();
-  bool isBtn4();
-  bool isBtn5();
-  bool isBtn6();
-  bool isBtn7();
-  bool isBtn8();
-  bool isBtn9();
-  bool isBtnAsterisk();
-  bool isBtn0();
-  bool isBtnHash();
-  bool isBtnUp();
-  bool isBtnLeft();
-  bool isBtnOk();
-  bool isBtnRight();
-  bool isBtnDown();
-
-private:
-  int recvPin;
-  bool isCommand(uint8_t command);
-  enum Command : uint8_t {
-    BTN_1 = 0x45,
-    BTN_2 = 0x46,
-    BTN_3 = 0x47,
-    BTN_4 = 0x44,
-    BTN_5 = 0x40,
-    BTN_6 = 0x43,
-    BTN_7 = 0x07,
-    BTN_8 = 0x15,
-    BTN_9 = 0x09,
-    BTN_ASTHERISK = 0x16,
-    BTN_0 = 0x19,
-    BTN_HASH = 0x0D,
-    BTN_UP = 0x18,
-    BTN_LEFT = 0x08,
-    BTN_OK = 0x1C,
-    BTN_RIGHT = 0x5A,
-    BTN_DOWN = 0x52
-  };
+  void resume();
+  bool isCommand(Command command);
 };
 
 #endif
